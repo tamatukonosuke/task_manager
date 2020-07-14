b768888e1495bf6e631aeeb2d86b280fd0dff7be
tamatsukonosuke <rao.koo621@icloud.com>
2020-07-14 17:23:51 +0900

    ユーザー認証機能追加する

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.urls import path
+from django.contrib.auth.views import logout
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')), 
     url(r'^admin/', admin.site.urls),
+    url('', include(('task_management_app.urls','task_management_app'))),
     url(r'^task_management_app/', include(('task_management_app.urls','task_management_app'))),
 ]
