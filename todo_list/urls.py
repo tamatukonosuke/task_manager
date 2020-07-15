59c06f2f378c194c42e1a2e9349165cfdb6e0a0a
tamatsukonosuke <rao.koo621@icloud.com>
2020-07-15 12:22:11 +0900

    todoに編集機能追加する

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('delete/<list_id>', views.delete, name="delete"),
     path('uncomplete/<list_id>', views.uncomplete, name="uncomplete"),
     path('complete/<list_id>', views.complete, name="complete"),
+    path('edit/<list_id>', views.edit, name="edit"),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -43,3 +43,17 @@ def complete(request, list_id):
     item.completed = True
     item.save()
     return redirect('todo_list:home')
+
+def edit(request, list_id):
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+        form = ListForm(request.POST or None, instance=item)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, ('Item Has Been Edited!'))
+            return redirect('todo_list:home')
+    else:
+        item = List.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'item': item})
