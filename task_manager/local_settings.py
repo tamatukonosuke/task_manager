b6392cc0b750eca1989c7081876176875eafc530
tamatsukonosuke <rao.koo621@icloud.com>
2020-11-04 15:44:45 +0900

    k

--- task_manager/local_settings.py
+++ task_manager/local_settings.py
@@ -9,6 +9,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'host',
+        'PORT': '',
     }
 }
 
